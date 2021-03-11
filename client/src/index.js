158009ef7858bb0cb48a2bdbae0f7475fed2495e
Vitali Griunvald <vgriunvald@gmail.com>
2021-03-11 23:45:39 +0200

    Project setup

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,6 @@ import App from './ui/App';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import store from './store/store';
-import 'semantic-ui-css/semantic.min.css';
 
 const configureStore = store();
 
