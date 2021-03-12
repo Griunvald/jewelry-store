eab9a56f6feb5830a9380d688a5ab5275a6b519d
Vitali Griunvald <vgriunvald@gmail.com>
2021-03-12 15:02:34 +0200

    Cahnge app theme

--- client/src/ui/components/Header.js
+++ client/src/ui/components/Header.js
@@ -11,7 +11,7 @@ const Header = () => {
         </LinkContainer>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
+          <Nav className="ml-auto">
             <IndexLinkContainer to="/">
               <Nav.Link>STORE</Nav.Link>
             </IndexLinkContainer>
