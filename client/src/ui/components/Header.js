cadd29c43766b6d826afc1d89e46c03b05ba3174
Vitali Griunvald <vgriunvald@gmail.com>
2021-03-12 20:11:38 +0200

    Add custom hamburger icon

--- client/src/ui/components/Header.js
+++ client/src/ui/components/Header.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 import { LinkContainer, IndexLinkContainer } from 'react-router-bootstrap';
+import Hamburger from 'hamburger-react';
 
 const Header = () => {
+  const [isOpen, setOpen] = useState(false);
+
   return (
     <>
       <Navbar bg="light" expand="lg" collapseOnSelect>
         <LinkContainer to="/">
           <Navbar.Brand>JEWELRY STORE</Navbar.Brand>
         </LinkContainer>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <span>
+            <Hamburger color="#000000" toggled={isOpen} toggle={setOpen} />
+          </span>
+        </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
             <IndexLinkContainer to="/">
