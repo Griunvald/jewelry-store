175f7867071e0b4811a81e90d12e76539033d756
Vitali Griunvald <vgriunvald@gmail.com>
2021-03-21 20:08:33 +0200

    [server] Move dotenv init function to the indexjs, reconfigure seeder

--- server/data/seeder.js
+++ server/data/seeder.js
@@ -1,12 +1,28 @@
-require('dotenv').config();
 const mongoose = require('mongoose');
 const users = require('./users');
 const items = require('./items');
 const User = require('../models/userModel');
 const Item = require('../models/itemModel');
 const Order = require('../models/orderModel');
-const connectDB = require('../config/db');
 
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(
+      'mongodb://localhost:27017/jewelry_store',
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+      }
+    );
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
 connectDB();
 
 const importData = async () => {
