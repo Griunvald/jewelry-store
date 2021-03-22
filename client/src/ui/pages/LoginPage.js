6ded427286ca8f34eeee327987b040d03feb5569
Vitali Griunvald <vgriunvald@gmail.com>
2021-03-23 01:59:48 +0200

    Add login form

--- client/src/ui/pages/LoginPage.js
+++ client/src/ui/pages/LoginPage.js
@@ -1,7 +1,73 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Form, Button, Col, Row } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { login } from '../../store/actions/authActions';
 
-const SignupPage = () => {
-  return <div>Signup page</div>;
+const SignupPage = ({ location, history }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const dispatch = useDispatch();
+  const auth = useSelector((state) => state.auth);
+  const { loading, error, userInfo } = auth;
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(login(email, password));
+  };
+
+  const redirect = location.search ? location.search.split('=')[1] : '/';
+
+  useEffect(() => {
+    if (userInfo) {
+      history.push(redirect);
+    }
+  }, [history, userInfo, redirect]);
+
+  return (
+    <Container className="d-flex justify-content-center">
+      <Col xs={12} md={6}>
+        <h2>Log in</h2>
+        {error && <h3>{error}</h3>}
+        <Form onSubmit={submitHandler}>
+          <Form.Group controlId="email">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+
+          <Form.Group controlId="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter your password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <Button type="submit" variant="dark" block size="lg">
+            Log in
+          </Button>
+        </Form>
+        <Row className="py-3">
+          <Col>
+            New Customer?{' '}
+            <Link
+              className="link"
+              to={redirect ? `/register?redirect=${redirect}` : '/register'}
+            >
+              Register
+            </Link>
+          </Col>
+        </Row>
+      </Col>
+    </Container>
+  );
 };
 
 export default SignupPage;
