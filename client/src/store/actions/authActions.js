c6028532e0b75485db3dc3c5880137f2cdd420b2
Vitali Griunvald <vgriunvald@gmail.com>
2021-03-23 22:30:59 +0200

    [client] Remove headers from axios post request

--- client/src/store/actions/authActions.js
+++ client/src/store/actions/authActions.js
@@ -4,11 +4,10 @@ import { LOGIN_SUCCESS, LOGIN_ERROR, LOGIN_REQUEST } from '../types';
 export const login = (email, password) => async (dispatch) => {
   try {
     dispatch({ type: LOGIN_REQUEST });
-    const { data } = await axios.post(
-      '/api/v1/users/login',
-      { email, password },
-      { headers: { 'Content-Type': 'application/json' } }
-    );
+    const { data } = await axios.post('/api/v1/users/login', {
+      email,
+      password,
+    });
     dispatch({ type: LOGIN_SUCCESS, payload: data });
     localStorage.setItem('userInfo', JSON.stringify(data));
   } catch (error) {
