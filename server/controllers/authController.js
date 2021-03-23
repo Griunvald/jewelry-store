eac70cfb305f86e868dd6cee38bf4cbe4fb1bc85
Vitali Griunvald <vgriunvald@gmail.com>
2021-03-23 21:38:28 +0200

    Add protect middleware and user profile route

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -56,3 +56,18 @@ exports.signup = Async(async (req, res) => {
     throw new Error('Invalid user data');
   }
 });
+
+exports.getUserProfile = Async(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404).json({ message: 'User not found' });
+  }
+});
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const protected = require('../middleware/protect_middleware');
 
 const authController = require('../controllers/authController');
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 // router.get('/logout', authController.logout);
+
+router.get('/profile', protected, authController.getUserProfile);
 
 module.exports = router;
